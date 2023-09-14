7f31e715b3ae4976e002bf94225d3a2c5c53a7cf
Paolo Billordo <paolobillordo@gmail.com>
2023-09-14 01:31:54 -0300

    tratando de incluir servers en chat

--- scripts/chat.js
+++ scripts/chat.js
@@ -1,5 +1,33 @@
 document.getElementById("logout").addEventListener("click", logout);
+window.addEventListener("load", function () {
+     get_servers();
+});
 
+function get_servers() {
+     const url = "http://127.0.0.1:5000/servers/user";
+     fetch(url, {
+          method: "GET",
+          credentials: "include",
+     })
+          .then((response) => {
+               if (response.status === 200) {
+                    return response.json().then((data) => {
+                         for (let i = 0; i < data.length; i++) {
+                              content.servers += `<img src="${data[i].icono}" alt="Image servidor"> <br> <h2>${data[i].name_server}</h2> <br>`;
+                         }
+                    });
+               } else {
+                    return response.json().then((data) => {
+                         document.getElementById("message").innerHTML =
+                              data.message;
+                    });
+               }
+          })
+          .catch((error) => {
+               document.getElementById("message").innerHTML =
+                    "Ocurrió un error.";
+          });
+}
 
 function logout() {
      const url = "http://127.0.0.1:5000/users/logout";
@@ -25,3 +53,14 @@ function logout() {
                     "Ocurrió un error.";
           });
 }
+
+const content = {};
+content.servers = '';
+
+document.addEventListener('DOMContentLoaded', () => {
+  let html = document.body.innerHTML;
+  Object.entries(content).forEach(([tag, data]) => {
+    html = html.replaceAll(`{${tag}}`, data);
+  });
+  document.body.innerHTML = html;
+});
