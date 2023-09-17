3361c88a059d1bb1dd9e1003abd9538c729674bd
CristianArias <cristianarias813@gmail.com>
2023-09-16 23:41:28 -0300

    Creaccion profile.

---
+++ scripts/profile.js
@@ -0,0 +1,35 @@
+const perfil = document.getElementById("perfil")
+if (perfil){ 
+    perfil.addEventListener("click", () => {
+        window.location.href = "profile.html"
+    })
+}
+
+window.addEventListener("load", function () {
+    get_profile();
+});
+
+function get_profile() {
+    const url = "http://127.0.0.1:5000/users/profile";
+
+    fetch(url, {
+        method: "GET",
+        credentials: "include",
+    })
+        .then((response) => response.json())
+        .then((data) => {
+            const nick_name = document.getElementById("nick")
+            nick_name.textContent = data.nick_name
+            const first_name = document.getElementById("name")
+            first_name.textContent = data.first_name
+            const last_name = document.getElementById("last")
+            last_name.textContent = data.last_name
+            const email = document.getElementById("email1")
+            email.textContent = data.email
+
+        })
+        .catch((error) => {
+            document.getElementById("message").innerHTML =
+                "Ocurrió un error.";
+        });
+}
